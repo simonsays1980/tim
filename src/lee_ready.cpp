fd4977cd910c731ec4ed8ba1fa22360ff808d9f9
simon.zehnder@gmail.com <>
2018-06-04 16:24:35 +0200

    Changed function and argument names.

--- src/lee_ready.cpp
+++ src/lee_ready.cpp
@@ -1,43 +1,46 @@
-#include "RcppArmadillo.h"
+#include <Rcpp.h>
 using namespace Rcpp;
 
-// [[Rcpp::depends(RcppArmadillo)]]
-
 // [[Rcpp::export]]
-IntegerVector lee_ready_vector( NumericVector price, NumericVector bidprice, 
-                                NumericVector askprice ) {
+IntegerVector lee_ready_vector( NumericVector &price, NumericVector &bidprice, 
+                                NumericVector &askprice ) {
   const int nrows = price.length();
+  IntegerVector indicator( nrows );
+  if ( nrows < 3 ) {
+    return indicator;
+  }
   if ( nrows != bidprice.length() || nrows != askprice.length() ) {
     throw std::invalid_argument( "Arguments differ in lengths" );
   }
-  IntegerVector indicator( nrows );
+  
   NumericVector midprice = ( askprice + bidprice ) / 2.0;
   
-  for( int i = 2; i <= nrows; ++i ) {
-    if ( price[i] == askprice[i] ) {
-      indicator[i] = 1;
-    } else if ( price[i] == bidprice[i] ) {
-      indicator[i] = -1;
+  for( int i = 0; i < nrows - 2; ++i ) {
+    if ( price[i+2] == askprice[i+2] ) {
+      indicator[i+2] = 1;
+    } else if ( price[i+2] == bidprice[i+2] ) {
+      indicator[i+2] = -1;
     } else {
-      if ( price[i] > midprice[i] ) {
-        indicator[i] = 1;
-      } else if ( price[i] < midprice[i] ) {
-        indicator[i] = -1;
+      if ( price[i+2] > midprice[i+2] ) {
+        indicator[i+2] = 1;
+      } else if ( price[i+2] < midprice[i+2] ) {
+        indicator[i+2] = -1;
       } else { 
         /* price == midpice */
-        if ( price[i] > price[i-1] ) {
-          indicator[i] = 1;
-        } else if ( price[i] < price[i-1] ) {
-          indicator[i] = -1;
+        if ( price[i+2] > price[i+1] ) {
+          indicator[i+2] = 1;
+        } else if ( price[i+2] < price[i+1] ) {
+          indicator[i+2] = -1;
         } else {
-          if ( price[i] > price[i-2] ) {
-            indicator[i] = 1;
+          if ( price[i+2] > price[i] ) {
+            indicator[i+2] = 1;
           } else {
-            indicator[i] = -1;
+            indicator[i+2] = -1;
           }
         }
       }
     }
   }
+  
   return indicator;
 }
