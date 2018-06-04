512365cc31b4d36d138f1ec8cc780534f304c26f
simon.zehnder@gmail.com <>
2018-06-04 16:23:41 +0200

    Changed function names and argument names.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,68 +7,73 @@
 using namespace Rcpp;
 
 // lee_ready_vector
-IntegerVector lee_ready_vector(NumericVector price, NumericVector bidprice, NumericVector askprice);
+IntegerVector lee_ready_vector(NumericVector& price, NumericVector& bidprice, NumericVector& askprice);
 RcppExport SEXP _tim_lee_ready_vector(SEXP priceSEXP, SEXP bidpriceSEXP, SEXP askpriceSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type price(priceSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type bidprice(bidpriceSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type askprice(askpriceSEXP);
+    Rcpp::traits::input_parameter< NumericVector& >::type price(priceSEXP);
+    Rcpp::traits::input_parameter< NumericVector& >::type bidprice(bidpriceSEXP);
+    Rcpp::traits::input_parameter< NumericVector& >::type askprice(askpriceSEXP);
     rcpp_result_gen = Rcpp::wrap(lee_ready_vector(price, bidprice, askprice));
     return rcpp_result_gen;
 END_RCPP
 }
-// rcpparma_hello_world
-arma::mat rcpparma_hello_world();
-RcppExport SEXP _tim_rcpparma_hello_world() {
+// nocb_1min_vector
+NumericVector nocb_1min_vector(NumericVector& price, newDatetimeVector& tradetime);
+RcppExport SEXP _tim_nocb_1min_vector(SEXP priceSEXP, SEXP tradetimeSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpparma_hello_world());
+    Rcpp::traits::input_parameter< NumericVector& >::type price(priceSEXP);
+    Rcpp::traits::input_parameter< newDatetimeVector& >::type tradetime(tradetimeSEXP);
+    rcpp_result_gen = Rcpp::wrap(nocb_1min_vector(price, tradetime));
     return rcpp_result_gen;
 END_RCPP
 }
-// rcpparma_outerproduct
-arma::mat rcpparma_outerproduct(const arma::colvec& x);
-RcppExport SEXP _tim_rcpparma_outerproduct(SEXP xSEXP) {
+// nocb_1min_dataframe
+DataFrame nocb_1min_dataframe(NumericVector& price, newDatetimeVector& tradetime);
+RcppExport SEXP _tim_nocb_1min_dataframe(SEXP priceSEXP, SEXP tradetimeSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_outerproduct(x));
+    Rcpp::traits::input_parameter< NumericVector& >::type price(priceSEXP);
+    Rcpp::traits::input_parameter< newDatetimeVector& >::type tradetime(tradetimeSEXP);
+    rcpp_result_gen = Rcpp::wrap(nocb_1min_dataframe(price, tradetime));
     return rcpp_result_gen;
 END_RCPP
 }
-// rcpparma_innerproduct
-double rcpparma_innerproduct(const arma::colvec& x);
-RcppExport SEXP _tim_rcpparma_innerproduct(SEXP xSEXP) {
+// nocb_5min_vector
+NumericVector nocb_5min_vector(NumericVector& price, newDatetimeVector& tradetime);
+RcppExport SEXP _tim_nocb_5min_vector(SEXP priceSEXP, SEXP tradetimeSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_innerproduct(x));
+    Rcpp::traits::input_parameter< NumericVector& >::type price(priceSEXP);
+    Rcpp::traits::input_parameter< newDatetimeVector& >::type tradetime(tradetimeSEXP);
+    rcpp_result_gen = Rcpp::wrap(nocb_5min_vector(price, tradetime));
     return rcpp_result_gen;
 END_RCPP
 }
-// rcpparma_bothproducts
-Rcpp::List rcpparma_bothproducts(const arma::colvec& x);
-RcppExport SEXP _tim_rcpparma_bothproducts(SEXP xSEXP) {
+// nocb_5min_dataframe
+DataFrame nocb_5min_dataframe(NumericVector& price, newDatetimeVector& tradetime);
+RcppExport SEXP _tim_nocb_5min_dataframe(SEXP priceSEXP, SEXP tradetimeSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_bothproducts(x));
+    Rcpp::traits::input_parameter< NumericVector& >::type price(priceSEXP);
+    Rcpp::traits::input_parameter< newDatetimeVector& >::type tradetime(tradetimeSEXP);
+    rcpp_result_gen = Rcpp::wrap(nocb_5min_dataframe(price, tradetime));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
     {"_tim_lee_ready_vector", (DL_FUNC) &_tim_lee_ready_vector, 3},
-    {"_tim_rcpparma_hello_world", (DL_FUNC) &_tim_rcpparma_hello_world, 0},
-    {"_tim_rcpparma_outerproduct", (DL_FUNC) &_tim_rcpparma_outerproduct, 1},
-    {"_tim_rcpparma_innerproduct", (DL_FUNC) &_tim_rcpparma_innerproduct, 1},
-    {"_tim_rcpparma_bothproducts", (DL_FUNC) &_tim_rcpparma_bothproducts, 1},
+    {"_tim_nocb_1min_vector", (DL_FUNC) &_tim_nocb_1min_vector, 2},
+    {"_tim_nocb_1min_dataframe", (DL_FUNC) &_tim_nocb_1min_dataframe, 2},
+    {"_tim_nocb_5min_vector", (DL_FUNC) &_tim_nocb_5min_vector, 2},
+    {"_tim_nocb_5min_dataframe", (DL_FUNC) &_tim_nocb_5min_dataframe, 2},
     {NULL, NULL, 0}
 };
 
